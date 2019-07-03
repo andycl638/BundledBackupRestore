8854182bc010758290638d777a1ffe266ab4d79b
Andy <andy@andys-mbp-3.almaden.ibm.com>
2019-07-03 07:48:34 -0700

    fixing bug

--- dirgen.py
+++ dirgen.py
@@ -11,7 +11,10 @@ def generate_dir(level3Name, level3Num, level4Name, level4Num, level5Name, level
     5 levels
     """
     initDir = os.path.join(root, level3Name + str(1), level4Name + str(1))
-    print("Initial Dir: %s" %initDir)
+    level3Dir = os.path.join(root, level3Name + str(1))
+    level4Dir = os.path.join(level3Dir, level4Name + str(1))
+    print("Initial Dir3: %s" %level3Dir)
+    print("Initial Dir4: %s" %level4Dir)
 
     os.makedirs(initDir)
     level3Num -= 1
@@ -20,20 +23,24 @@ def generate_dir(level3Name, level3Num, level4Name, level4Num, level5Name, level
     for num in range(1, level5Num+1):
         os.makedirs(os.path.join(initDir, level5Name + str(num)))
         print("making lvl 5 dirs")
-        print(os.path.join(initDir, level5Name + str(num)))
+        print(os.path.join(level4Dir, level5Name + str(num)))
 
     """
     Copy initial directory structure to level 4 directory
     """
     for num in range(2, level4Num + 2):
         print("copy level 4 dir tree")
-        print(os.path.join(level3Name + str(1), level4Name + str(num)))
-        shutil.copytree(initDir, os.path.join(level3Name + str(1), level4Name + str(num)))
+        print("copy %s" %level4Dir)
+        s = os.path.join(level3Dir, level4Name + str(num))
+        print("to %s" %s)
+        shutil.copytree(level4Dir, os.path.join(evel3Dir, level4Name + str(num)))
 
     for num in range(2, level3Num + 2):
         print("copy level 3 dir tree")
-        print(os.path.join(level3Name + str(num)))
-        shutil.copytree(initDir, level3Name + str(1), level3Name + str(num))
+        print("copy %s" %level3Dir)
+        s = os.path.join(root, level3Name + str(num))
+        print("to %s" %s)
+        shutil.copytree(level3Dir, os.path.join(root, level3Name + str(num)))
 
 def generate_dir_list(foldernum):
     file = open("directory list", "w")
@@ -49,11 +56,11 @@ def generate_dir_list(foldernum):
 if __name__ == '__main__':
     root = "/vz9"
     level3Name = "Level3-"
-    level3Num = 1
+    level3Num = 2
     level4Name = "Level4-"
     level4Num = 2
     level5Name = "Level5-"
-    level5Num = 1
+    level5Num = 2
     generate_dir(level3Name, level3Num, level4Name, level4Num, level5Name, level5Num, root)
 """
     root = "/vsnap/vpool1/vz7"
