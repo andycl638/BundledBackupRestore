4bd1cc18737cab5033004d830d40584d0268b1ac
Andy <andy@andys-mbp-3.almaden.ibm.com>
2019-09-04 10:05:51 -0700

    testing

--- tester2.py
+++ tester2.py
@@ -36,6 +36,91 @@ def dosomething2(list):
 def rundsmc():
     print("dsmc")
 
+def test1():
+    print("\nget a list of all the dirs that needs to be backed up")
+    print("path: /vz6")
+    start = time.time()
+    dir_list = []
+    set_list = []
+    multi_set = []
+    dir_size = 0
+    total_size = 0
+
+    for root, dirs, files in os.walk('/vz6'):
+        print(root)
+        for file in files:
+            file_path=os.path.join(root, file)
+            dir_size += os.path.getsize(file_path)
+
+        set_list.append(root)
+        set_list.append("dest")
+        set_list.append(dir_size)
+        dir_list.append(set_list)
+        set_list = []
+        total_size += dir_size
+        dir_size = 0
+
+
+    end = time.time()
+    elapsed = end - start
+    print("Time to gather all files: %s" %elapsed)
+    print(dir_list)
+
+def test2():
+    print("\nget a list of all the dirs that needs to be backed up")
+    print("path: /vz6")
+    start = time.time()
+    dir_size = 0
+
+    for root, dirs, files in os.walk('/Users/andy/Documents/tester'):
+        print(root)
+        arr = os.listdir(root)
+        print(arr)
+
+    end = time.time()
+    elapsed = end - start
+    print("Time to gather all files: %s" %elapsed)
+
+def test3():
+    '''
+        Prepares a list of directories that will be bundled
+
+        Returns
+            dir_list    -- list of directories
+            total_size  -- the total size of data that will be backed up
+    '''
+    print("\nget a list of all the dirs that needs to be backed up")
+    print("path: '/Users/andy/Documents/tester'")
+    start = time.time()
+    dir_list = []
+    set_list = []
+    multi_set = []
+    dir_size = 0
+    total_size = 0
+
+    for root, dirs, files in os.walk('/Users/andy/Documents/tester'):
+        for dir in dirs:
+            dir_path=os.path.join(root, dir)
+
+            for file in os.listdir(dir_path):
+                file_path=os.path.join(dir_path, file)
+                if os.path.isfile(file_path):
+                    dir_size += os.path.getsize(file_path)
+
+            set_list.append(dir_path)
+            set_list.append("dest")
+            set_list.append(dir_size)
+            dir_list.append(set_list)
+            set_list = []
+            total_size += dir_size
+            dir_size = 0
+
+    end = time.time()
+    elapsed = end - start
+    print("Time to gather all files: %s" %elapsed)
+    print("Got all dirs and size")
+    print(dir_list)
+    return dir_list, total_size
 
 if __name__ == '__main__':
     testarr = [1,2,3,4,5,6,7,8,9]
@@ -60,28 +145,21 @@ if __name__ == '__main__':
     print(q.get())
     p2.join()'''
 
-    print("\nget a list of all the dirs that needs to be backed up")
-    print("path: /vz6")
-    start = time.time()
-    dir_size = 0
+    test3()
 
-
-    for root, dirs, files in os.walk('/vz6'):
-        for dir in dirs:
-            dir_path=os.path.join(root, dir)
-
-            for file in os.listdir(dir_path):
-                file_path=os.path.join(dir_path, file)
-                if os.path.isfile(file_path):
-                    dir_size += os.path.getsize(file_path)
-
-            print(dir_path)
-            print(dir_size)
-            dir_size = 0
-
-    end = time.time()
-    elapsed = end - start
-    print("Time to gather all files: %s" %elapsed)
+'''
+[['/Users/andy/Documents/tester/untitled folder', 'dest', 5254493],
+['/Users/andy/Documents/tester/vz8', 'dest', 7350],
+['/Users/andy/Documents/tester/group1', 'dest', 6148],
+['/Users/andy/Documents/tester/3', 'dest', 6148],
+['/Users/andy/Documents/tester/2', 'dest', 6148],
+['/Users/andy/Documents/tester/untitled folder/sdw copy', 'dest', 3145728],
+['/Users/andy/Documents/tester/untitled folder/sdw', 'dest', 3145728],
+['/Users/andy/Documents/tester/group1/filer1', 'dest', 6148],
+['/Users/andy/Documents/tester/group1/filer1/vz8', 'dest', 0],
+['/Users/andy/Documents/tester/3/asfdasd', 'dest', 9565],
+['/Users/andy/Documents/tester/3/asfdasd/wert3', 'dest', 0],
+['/Users/andy/Documents/tester/2/zdaa', 'dest', 0]]'''
 
 
 
