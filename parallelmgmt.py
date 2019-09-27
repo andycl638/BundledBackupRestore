e416295a645be47a9db71e8f69283af3ac8e22c6
Andy Cheong <acheong@us.ibm.com>
2019-09-27 09:17:00 -0700

    adding backup

--- parallelmgmt.py
+++ parallelmgmt.py
@@ -83,7 +83,7 @@ class ParallelMgmt():
                 dsmc.write_virtualmnt()
                 backup = dsmc.backup(backup_list)
 
-                #transfer_rate = dsmc.cmd(backup)
+                transfer_rate = dsmc.cmd(backup)
                 transfer_rate = 10000
                 transfer_rate_mib = float(transfer_rate)/1024
                 aggregate = (total_throughput + transfer_rate_mib)/2
