f5506eb4f845a8ce0b38de1a3c0164aa3beed5fd
Andy Cheong <acheong@us.ibm.com>
2019-10-03 07:58:10 -0700

    adding test script

--- filegen.py
+++ filegen.py
@@ -3,12 +3,12 @@ import subprocess
 from multiprocessing import Pool
 import time
 
-def generate_big_random_bin_file(filename):
+def generate_big_random_bin_file(filename, size):
     """
     generate big binary file with the specified size in bytes
     :param filename: the filename
     """
-    size = 1024*1024
+    #size = 1024*1024
     with open('%s'%filename, 'wb') as fout:
         fout.write(os.urandom(size)) #1
 
@@ -256,8 +256,11 @@ if __name__ == '__main__':
     level5Name = "Level5-"
     level5Num = 2
     lvl5FileNum = 10
-    generate_files_walk_dir("/vsnap/vpool1/vz8", 10)
-    #generate_big_random_bin_file("testfile.txt", 1024*1024)
+    #generate_files_walk_dir("/vsnap/vpool1/vz8", 10)
+    generate_big_random_bin_file("/vz8/1/testfile.txt", 1024)
+    generate_big_random_bin_file("/vz8/2/testfile2.txt", 1024)
+    generate_big_random_bin_file("/vz8/4/testfil4.txt", 1024)
+    generate_big_random_bin_file("/vz8/6/testfile6.txt", 1024)
     #parallel_file_gen(level3Name, level3Num, lvl3FileNum, level4Name, level4Num, lvl4FileNum, level5Name, level5Num, lvl5FileNum, root)
     #generate_file_ldeedee_unique(/vz9/)
     #s = parallel_file_gen2("/vz8",  10)
