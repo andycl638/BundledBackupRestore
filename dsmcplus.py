3c7729c2f704682d4657ef1f7ae54ded05d7b634
Andy Cheong <acheong@us.ibm.com>
2019-10-03 14:50:06 -0700

    fix stuff

--- dsmcplus.py
+++ dsmcplus.py
@@ -88,7 +88,7 @@ def mainrestore(args):
 
     dsmc = DsmcWrapper('', args.resourceutilization, '', '', unbundler.src_path)
     controller = ParallelMgmt(int(args.parallelism), args.destination, source_path)
-    return_q, elapsed = controller.start_controller(unbundler, dsmc)
+    return_q, elapsed = controller.start_controller_res(unbundler, dsmc)
     #restore = dsmc.restore()
 #    transfer_rate = dsmc.cmd(restore)
 
